99a6bba2e1d2084d685fb38621e23f63c8076395
hadiofficialdev <hadiofficialdev@gmail.com>
2026-05-13 08:29:26 +0500

    dark theme added

---
+++ src/darktheme.js
@@ -0,0 +1,32 @@
+
+const themeToggle = document.querySelector("#theme_toggle");
+const logoLight = document.querySelector(".logo");
+const logoDark = document.querySelector(".footerlogo");
+
+const theme = localStorage.getItem("theme");
+
+// apply theme on load
+if (theme === "dark") {
+  document.body.classList.add("dark_theme");
+  themeToggle.checked = true;
+
+  logoLight.src = "./public/images/logoFooterfinal-removebg-preview.png";
+  logoDark.src = "./public/images/logo-removebg-preview.png";
+}
+
+// toggle theme
+themeToggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark_theme");
+
+  const isDark = document.body.classList.contains("dark_theme");
+
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+
+  if (isDark) {
+    logoLight.src = "./public/images/logoFooterfinal-removebg-preview.png";
+    logoDark.src = "./public/images/logo-removebg-preview.png";
+  } else {
+    logoLight.src = "./public/images/logo-removebg-preview.png";
+    logoDark.src = "./public/images/logoFooterfinal-removebg-preview.png";
+  }
+});
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './style.css'
+import "./darktheme";
 
 import products from "./api/products.json";
 import { showProductContainer } from './homeProductCards';
+
 
 console.log(products);
 showProductContainer(products);
